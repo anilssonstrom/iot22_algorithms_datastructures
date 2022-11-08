6624cac14f91c5ffb0adff60b4ba1734fff1715b
Andreas Nilsson Ström <andreas.nilsson@natrolit.se>
2022-11-08 14:41:52 +0100

    Update lecture 1, add assignment 1

--- Lectures/Lecture 1/linked_list.py
+++ Lectures/Lecture 1/linked_list.py
@@ -11,40 +11,90 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def __str__(self):
-        current_node = self.head
+        node = self.head
         my_str = ""
 
-        while current_node is not None:
-            my_str += f"{current_node.data} -> "
-            current_node = current_node.next
+        while node is not None:
+            my_str += f"{node.data} -> "
+            node = node.next
 
         my_str += "None"
         return my_str
+
+    def count(self):
+        node = self.head
+        n = 0
+
+        while node is not None:
+            # Lägga på 1 på vår räknare
+            n += 1
+            # Gå vidare till nästa node
+            node = node.next
+
+        return n
+
+    def sum(self):
+        node = self.head
+        s = 0
+
+        while node is not None:
+            s += node.data
+            node = node.next
+
+        return s
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None or self.tail is None:
+            # List was empty
+            self.head = new_node
+            self.tail = new_node
+        else:
+            # List is not empty, just add the node to the end
+            self.tail.next = new_node
+            self.tail = new_node
 
 
 if __name__ == '__main__':
     llist = LinkedList()
     print(llist)
 
-    box1 = Node("a")
-    llist.head = box1
+    # box1 = Node(1)
+    # llist.head = box1
+    llist.append(1)
     print(llist)
 
-    box2 = Node("b")
-    box1.next = box2
+    # box2 = Node(2)
+    # box1.next = box2
+    llist.append(2)
     print(llist)
 
-    box3 = Node("c")
-    box2.next = box3
+    # box3 = Node(3)
+    # box2.next = box3
+    llist.append(3)
     print(llist)
 
-    current_node = llist.head
+    node = llist.head
     # Börja på första noden och gå till next, tills vi kommer till slutet
-    while current_node is not None:
+    while node is not None:
         # Skriva ut vad noden innehåller
-        print(current_node.data)
+        print(node.data)
 
         # Gå vidare till nästa nod
-        current_node = current_node.next
+        node = node.next
+
+    print("LList length: " + str(llist.count()))
+    assert llist.count() == 3
+
+    print("LList sum: " + str(llist.sum()))
+    assert llist.sum() == 6
+
+    print("Append(4)")
+    llist.append(4)
+    print("LList length: " + str(llist.count()))
+    print("LList sum: " + str(llist.sum()))
+    print(llist)
