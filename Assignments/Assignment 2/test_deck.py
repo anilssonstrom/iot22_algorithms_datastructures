3cfee7ca31b04ceec0d303cffb42b29e747f9faa
Andreas Nilsson Ström <andreas.nilsson@natrolit.se>
2022-11-14 14:59:32 +0100

    Add Assignment 2 and palindrome check for lecture 4

---
+++ Assignments/Assignment 2/test_deck.py
@@ -0,0 +1,13 @@
+from deck import Deck, Card
+
+# Implementera tester ni anser lämpliga här. Motivera gärna varför de behövs (vad de testar och varför).
+
+
+def test_card():
+    # Test här
+    pass
+
+
+def test_deck():
+    # Test här
+    pass
